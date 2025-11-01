1a1e5ed6671d7b582c97444ebb59d33e49930c47
Andrew Hwang <106andrew36@gmail.com>
2025-11-01 13:24:17 -0400

    Initial Commit

---
+++ yori/yoriApp.swift
@@ -0,0 +1,17 @@
+//
+//  yoriApp.swift
+//  yori
+//
+//  Created by Andrew Hwang on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct yoriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
